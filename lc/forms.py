f353569508f70ca8c494729923731a88742fa70e
Anatolii <mavtodrug@mail.ru>
2023-10-03 15:33:13 +0500

    #lesson 13  100%

--- lc/forms.py
+++ lc/forms.py
@@ -2,8 +2,8 @@ from django import forms
 from .models import *
 
 class AddPostForm(forms.Form):
-    title=forms.CharField(max_length=255)
-    slug=forms.SlugField(max_length=255)
-    content=forms.CharField(widget=forms.Textarea(attrs={'cols':60,'rows':10}))
-    is_published=forms.BooleanField()
-    cat=forms.ModelChoiceField(queryset=Category.objects.all())
+    title=forms.CharField(max_length=255, label='Заголовок', widget=forms.TextInput(attrs={'class':'form-input'}))
+    slug=forms.SlugField(max_length=255, label='URL')
+    content=forms.CharField(widget=forms.Textarea(attrs={'cols':60,'rows':10}),label='Статья')
+    is_published=forms.BooleanField(label='Публикация', required=False, initial=True)
+    cat=forms.ModelChoiceField(queryset=Category.objects.all(), label='Категория', empty_label='Категория не выбрана')
