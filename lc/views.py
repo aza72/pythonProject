37a8502696f865ea60c9e0d2858594cbd57288f7
Anatolii <mavtodrug@mail.ru>
2023-10-10 10:07:20 +0500

    #lesson 18  100%

--- lc/views.py
+++ lc/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.urls import reverse_lazy
@@ -11,6 +12,7 @@ from .utils import *
 
 
 class lcHome(DataMixin, ListView):
+
     model = users
     template_name = 'lc/index.html'
     context_object_name = 'posts'
@@ -37,7 +39,12 @@ class lcHome(DataMixin, ListView):
 #     return render(request, 'lc/index.html', context=index_param )
 
 def about (request):
-    return render(request,'lc/about.html',{'menu':menu,'title':'Страница о нас'})
+    contact_list = users.objects.all()
+    paginator = Paginator(contact_list, 3)  # Show 25 contacts per page.
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request,'lc/about.html',{'page_obj':page_obj,'menu':menu,'title':'Страница о нас'})
 
 class add_page(LoginRequiredMixin, DataMixin, CreateView):
     form_class = AddPostForm
@@ -105,6 +112,7 @@ class showpost(DataMixin, DetailView):
 
 
 class lcCategory(DataMixin, ListView):
+
     model = users
     template_name = 'lc/index.html'
     context_object_name = 'posts'
